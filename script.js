195ee75e7361b195f554c64da411055193e8a3bc
Emily Cain <you@example.com>
2021-11-20 18:38:12 -0500

    I think I'm done

--- script.js
+++ script.js
@@ -17,19 +17,20 @@ function writePassword() {
 
 var upperCase= [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z']
 var lowerCase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var number=[0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9]
-var special=['`','!','@','#','$','%','^','&','*','?']
+var number=[0,1,2,3,4,5,6,7,8,9]
+var special=['`','!','@','#','$','%','^','&','*','?','(','?']
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 var generatePassword=function(){
-  let prompt= window.prompt("Pick a number between 8 and 128, this will be your password length.")
-  if (prompt <8||prompt >128){
-    window.alert("For the last time, pick a number between 8 and 128.");
+  let passwordLength= window.prompt("Pick a number between 8 and 128, this will be your password length.")
+  if (passwordLength <8||passwordLength >128){
+    window.alert("For the last time, pick a number between 8 and 128. Start Over.");
+    return "";
   } 
-  else if (prompt >8|| prompt <128){
+  else if (passwordLength >8|| passwordLength <128){
     var confirmUpperCase= window.confirm ("UPPERCASE LETTERS???");
     var confirmLowerCase= window.confirm ("lowercase letters???");
-    var confirmNumber= window.confirm("How 'bout sum Numbers?Get it? SUM?");
+    var confirmNumber= window.confirm("How 'bout sum Numbers?");
     var confirmSpecial=window.confirm("Let's round it out with some special characters.");
   }
 //Generate the password time
@@ -47,9 +48,18 @@ var generatePassword=function(){
   }
   else if(confirmUpperCase&&confirmSpecial&&confirmNumber){
   output=[...upperCase,...special,...confirmNumber];  
+ } else{
+   window.alert("Start Over :/")
+   return"";
  }
 
- function outputRandom(password){
-   var 1= password.length;
+ //Generate password is "undefined". Create a shuffle.
+var randomPassword="";
+for (let index = 0; index < passwordLength; index++) {
+  var randomCharacterIndex= Math.floor(Math.random()*output.length)
+  randomPassword += output[randomCharacterIndex];
   
+}
+
+return randomPassword;  
  }
